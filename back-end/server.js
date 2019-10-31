c634160055681c25f28ce7fa619c2cf96dc03cf8
Pablo Sivilotti <psivilotti@fabrica1.corp>
2019-10-31 13:50:22 -0300

    ---

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const mongo = require('mongodb'), //el driver para poder conectar a mongodb
+// const mongoose = require('mongoose'); //el cliente para poder realizar consultas.
+// const Schema = mongoose.Schema; //la clase que permite definir "esquemas" de documento
+const bodyParser = require('body-parser'); //middleware para poder codificar datos enviados por POST en formato urlencoded, y para poder enviar el payload como json
+const path = require("path");
+
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+// .catch((err)=>{console.log(err); process.exit(1)});
+
+//+srv://pablosivilotti:<Pablito1.>@mytinerary-sjs0f.mongodb.net/test?retryWrites=true&w=majority
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,8 +16,16 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+  res.send('Bienvenido al Servidor' );
+});
+
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
+});
+
+app.get('/front', (req, res) => {
+  res.sendFile(path.join(process.cwd(), '/frontEnd/public/index.html'));
 });
 
 app.post('/api/world', (req, res) => {
