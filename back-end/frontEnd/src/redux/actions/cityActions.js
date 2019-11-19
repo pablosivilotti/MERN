7fe563a5cfec735a49559795fd6ce358f522c413
Pablo Sivilotti <psivilotti@fabrica1.corp>
2019-11-19 12:39:17 -0300

    implementacion redux-thunk

--- back-end/frontEnd/src/redux/actions/cityActions.js
+++ back-end/frontEnd/src/redux/actions/cityActions.js
@@ -1,14 +1,27 @@
+import axios from "axios";
+
 const addCity = payload => ({
   type: 'ADD_CITY',
   payload: payload
 })
-
 
 const deleteCity = payload => ({
   type: 'DELETE_CITY',
   payload: payload
 })
 
+function getCities() {
+  return (dispatch, getState) => {
+
+    if (getState().length > 0) return;
+
+    return axios.get('http://localhost:5000/cities')
+      .then((res) => {
+        dispatch({ type: 'GET_CITIES', payload: res.data.allCities })
+      })
+  }
+}
+
 export {
-  addCity, deleteCity
+  addCity, deleteCity, getCities
 }
--- back-end/frontEnd/src/redux/components/CitiesRedux.js
+++ back-end/frontEnd/src/redux/components/CitiesRedux.js
@@ -1,13 +1,9 @@
 import React from "react";
-import axios from "axios";
+// import axios from "axios";
 import { connect } from "react-redux";
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import { addCity } from "../actions/cityActions";
+import { getCities } from "../actions/cityActions";
 import { Navbar, Form } from 'react-bootstrap'
+import List from '@material-ui/core/List';
 
 
 
@@ -15,22 +11,13 @@ class HelloRedux extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      cities: [],
+      // cities: [],
       name: ''
     }
   }
 
-  async componentDidMount() {
-    await axios.get("http://localhost:5000/cities").then(res => {
-      const cities = res.data.allCities.map(obj => ({
-        id: obj._id,
-        name: obj.name,
-        country: obj.country
-      }));
-      // this.setState({ cities });
-      //dispatch addCity
-      this.props.dispatch(addCity(cities))
-    });
+  componentDidMount() {
+    this.props.getCities();
   }
 
   onChange2 = name => event => {
@@ -39,8 +26,8 @@ class HelloRedux extends React.Component {
       : event.target.value
 
     this.setState({ [name]: value_ })
-    console.log("onChange")
-    console.log(value_)
+    // console.log("onChange")
+    // console.log(value_)
   }
 
   render() {
@@ -52,52 +39,36 @@ class HelloRedux extends React.Component {
       <div>
 
         <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="./cities">Cities Album</Navbar.Brand>
+          <Navbar.Brand href="./cities">Cities Album</Navbar.Brand>
           {/* <Nav className="mr-auto">
           <Nav.Link href="./cities">Cities Album</Nav.Link>
 
           </Nav> */}
           <Form inline>
 
-          {/* <form className="form-inline search" > */}
-            <input className="form-control mr-sm-2" type="search" onChange={this.onChange2('name')} placeholder="Search cities" aria-label="Search" /*value={this.state.filterC} onChange2={console.log(this.state.filterC)}*/ />
-          {/* </form> */}
+            {/* <form className="form-inline search" > */}
+            <input className="form-control mr-sm-2" type="search" onChange={this.onChange2('name')} placeholder="Search cities" aria-label="Search" />
+            {/* </form> */}
           </Form>
 
         </Navbar>
 
 
-        <Table className="table">
-          <TableHead>
-            <TableRow>
-              {/* <TableCell align="center"> Id </TableCell> */}
-              <TableCell align="center"> City </TableCell>
-              <TableCell align="center"> Country </TableCell>
-            </TableRow>
-          </TableHead>
-
-
-          <TableBody>
-            {this.props.cities[0] && (this.props.cities[0].filter(function (city) {
-              return city.name.toLowerCase().indexOf(state1.toLowerCase()) !== -1
-            }).map(function (city, index) {
-              return (
-                <TableRow key={index}>
-                  {/* <TableCell align="center">{city.id}</TableCell> */}
-                  <TableCell align="center">{city.name}</TableCell>
-                  <TableCell align="center">{city.country}</TableCell>
-                </TableRow>
-              );
-            }))}
-          </TableBody>
-        </Table>
-
+          {this.props.cities[0] && (this.props.cities[0].filter(function (city) {
+            return city.name.toLowerCase().indexOf(state1.toLowerCase()) !== -1
+          }).map(function (city, index) {
+            return (
+              <List key={index}>
+                {city.name + ", " + city.country}
+              </List>
+            );
+          }))}
       </div>
     );
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   // console.log("state")
   // console.log(state)
   return {
@@ -111,5 +82,5 @@ const mapStateToProps = state => {
 //   getCities: getCitiesAction
 // };
 
-export default connect(mapStateToProps)(HelloRedux);
+export default connect(mapStateToProps, { getCities })(HelloRedux);
 // export default HelloRedux;
