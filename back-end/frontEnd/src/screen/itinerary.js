b908b79c8ef98653feca0c6286723b7a9917243d
Pablo Sivilotti <pablosivilotti@gmail.com>
2019-11-26 20:46:59 -0300

    -

--- back-end/frontEnd/src/screen/itinerary.js
+++ back-end/frontEnd/src/screen/itinerary.js
@@ -3,6 +3,8 @@ import '../App.css'
 import Footer from '../components/footer';
 import { connect } from "react-redux";
 import { getItineraries } from "../redux/actions/itineraryActions";
+import { getActivity } from "../redux/actions/activityActions";
+
 // import List from '@material-ui/core/List';
 import Card from 'react-bootstrap/Card'
 // import Collapse from 'react-bootstrap/Collapse'
@@ -10,6 +12,11 @@ import Accordion from 'react-bootstrap/Accordion'
 import Media from 'react-bootstrap/Media'
 // import img1 from '../img/GaudiLover.png'
 
+// function handleClick(event) {
+//  console.log("CLIC")
+//  console.log(this)
+// //  this.props.getActivity(this.props.match.params.itineraryId)
+// }
 
 class Itinerary extends React.Component {
 
@@ -17,7 +24,18 @@ class Itinerary extends React.Component {
     await this.props.getItineraries(this.props.match.params.cityId)
   }
 
-  //LLAMAR A GETACTIVITY AL CLICKEAR EN VIEW ALL PARA MOSTRAR LAS IMAGENES
+  handleClick() {
+    console.log("CLIC")
+    // this.props.dispatch(getActivity(this.props.match.params.cityId));
+  }
+  
+  // //LLAMAR A GETACTIVITY AL CLICKEAR EN VIEW ALL PARA MOSTRAR LAS IMAGENES
+  
+  // onCLicAccordion = () =>{
+  //   console.log("CLIC")
+  //   // console.log(this.props)
+  //   // this.props.getActivity(this.props.match.params.itineraryId)
+  // }
 
   render() {
 
@@ -25,11 +43,13 @@ class Itinerary extends React.Component {
 
       <div className="Redux">
 
-        {this.props.itineraryReducer[0] != null && 
-        <div> 
-          {/* <h1> {this.props.itineraryReducer[0][0].cityId.name} </h1> */}
-          <img src={`http://localhost:5000/city/image/${this.props.itineraryReducer[0][0].cityId.url}`} className="IMG-CITY-TITLE" alt="CITY"></img>
-        </div>
+        {this.props.itineraryReducer[0] != null &&
+          <Card>
+            <Card.Img src={`http://localhost:5000/city/image/${this.props.itineraryReducer[0][0].cityId.url}`} alt="Card image" />
+            <Card.ImgOverlay>
+              <Card.Title className="titleItin">{this.props.itineraryReducer[0][0].cityId.name}</Card.Title>
+            </Card.ImgOverlay>
+          </Card>
         }
 
         <h4 id="Availables-MYT"> Available MYtinerary: </h4>
@@ -50,9 +70,9 @@ class Itinerary extends React.Component {
                     <h5>{itineraries.title}</h5>
                     <h6> {"Likes: " + itineraries.rating + "     " + itineraries.duration + " Hs       $" + itineraries.price} </h6>
                     <h6> {itineraries.hashtag} </h6>
-                    <Accordion defaultActiveKey="0">
-                      <Media.Body className="ViewAllAccordion">
-                        <Accordion.Toggle as={Media.Body} variant="link" eventKey="1">
+                    <Accordion defaultActiveKey="0" onClick={() => this.handleClick()}>
+                      <Media.Body className="ViewAllAccordion" >
+                        <Accordion.Toggle as={Media.Body} variant="link" eventKey="1"  >
                           v View All v
                        </Accordion.Toggle>
                       </Media.Body>
